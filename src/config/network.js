7fa94901455c7a77cabee96e9b920b086a5eded4
lukax76-boop <lukax76@gmail.com>
2026-04-21 11:41:49 -0600

    agregado archivo npmrc

--- src/config/network.js
+++ src/config/network.js
@@ -1,3 +1,3 @@
 // Configuración de red para el servidor Socket.io
 // Reemplaza esto con tu IP local actual si cambia.
-export const SERVER_URL = 'http://192.168.0.84:3000';
+export const SERVER_URL = 'https://loteriamexicana.onrender.com';
