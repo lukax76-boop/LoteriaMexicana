8394617c0cec1c74c5d9c858f2858d8fa7e626c9
lukax76-boop <lukax76@gmail.com>
2026-05-02 12:27:44 -0600

    fix: onlinePlayers broadcast on disconnect

--- server/server.js
+++ server/server.js
@@ -508,6 +508,7 @@ io.on('connection', (socket) => {
 
   socket.on('disconnect', () => {
     console.log('User disconnected:', socket.id);
+    broadcastOnlinePlayers();
   });
 
   socket.on('recoverPassword', (data, callback) => {
